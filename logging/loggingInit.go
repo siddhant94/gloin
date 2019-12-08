94f010ec619bafc87836014475825116a51d672a
Siddhant Sinha <sid.sinha94@gmail.com>
2019-12-08 19:45:45 +0530

    download bing image to filesystem
    The filesystem path is provided as application parameter and
    the filename is the 'title' provided by Bing

--- logging/loggingInit.go
+++ logging/loggingInit.go
@@ -9,7 +9,7 @@ var Logger *log.Logger
 
 func init() {
 	// f, err := os.OpenFile("/logs/error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	f, err := os.OpenFile("/logs/error.log",
+	f, err := os.OpenFile("logging/error.log",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println(err)
--- main.go
+++ main.go
@@ -9,9 +9,13 @@ import (
 )
 
 func main() {
-	// argsWithProg := os.Args
+	if len(os.Args) <= 1 {
+		fmt.Println("Directory name missing")
+		return
+	}
 	userDirPath := os.Args[1] // Get arguments aside from pragram path
-	fmt.Println("If the directory mentioned is not present, this will create one")
+	fmt.Println("If the directory mentioned is not present, this will create one. Here all wallpapers will be" +
+		"downloaded in future...:)")
 	check, err := utils.Exists(userDirPath) // Checks whether file/dir exists or not
 	if err != nil {
 		//log.Fatalf("error creating/opening log file: %v", err)
@@ -25,5 +29,13 @@ func main() {
 			errLog.Logger.Printf("error creating directory: %v \n", err)
 			return
 		}
+	}
+	// We have directory path, now time for calling rest endpoint and gfetting wallpaper meta
+	metaData := utils.GetBingWallpaperMeta()
+	success, savedFile := utils.DownloadWallpaper(metaData, userDirPath)
+	if(success) {
+		fmt.Println("Finished ...Your wallpaper is downloaded and with filename "+ savedFile +" and is present at " + userDirPath)
+	} else {
+		fmt.Println("Uh oh!! Could not save Image file. Check error log")
 	}
 }
